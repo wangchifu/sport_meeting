c65315f8353fae81292079ed998b42f44fc998ab
wang <wang1026>
2020-12-11 15:43:21 +0800

    add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,24 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->id();
+            $table->string('uid')->nullable();//gsuite帶出uid
+            $table->string('edu_key')->nullable();//gsuite及cloudschool帶出edu_key
+            $table->string('name');//姓名
+            $table->string('email')->nullable();//email
+            $table->string('kind')->nullable();//gsuite帶出教職員、學生
+            $table->string('title')->nullable();//gsuite帶出職稱 即 cloudschool帶出的title_name
+            $table->string('code')->nullable();//gsuite帶出學校代碼 即 cloudschool 帶出的 school_no
+            $table->string('school')->nullable();//gsuite帶出學校名稱
+            $table->string('schools')->nullable();//gsuite帶出學校群組
+            $table->string('username')->nullable();//gsuite帳號
+            $table->string('password')->nullable();
+            $table->string('cloudschool_username')->nullable();//cloudschool帳號
+            $table->string('role')->nullable();//cloudschool 帶出 role
+            $table->string('title_kind')->nullable();//cloudschool 帶出 title_kind
+            $table->string('group')->nullable();//cloudschool 帶出 group
+            $table->tinyInteger('admin')->nullable();//停用
+            $table->tinyInteger('disable')->nullable();//停用
             $table->rememberToken();
             $table->timestamps();
         });
